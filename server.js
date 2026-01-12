382f3cf74505d9c667bb8224a695412bab1e1c91
Mawrcus <marcus@chargeduphomebuyers.com>
2026-01-11 20:35:21 -0500

    Server Tweak

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
     "http://localhost:5500",
     "http://127.0.0.1:5500"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
